8bc9aa3ab47350ae38fec94272e6cde82cfba66b
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2024-06-19 11:47:53 -0300

    curso nao esta sendo atualizado no vercel

--- src/app/components/Central/tela-editar-professor/tela-editar-professor.component.ts
+++ src/app/components/Central/tela-editar-professor/tela-editar-professor.component.ts
@@ -37,71 +37,41 @@ export class TelaEditarProfessorComponent implements OnInit {
   }
 
 
-  ngOnInit(): void {
-    const nome = this.route.snapshot.paramMap.get('nome') ?? '';
 
-    // Carrega o professor pelo nome e encadeia com a listagem de cursos
-    this.professorService.carregaProfessorPeloNome(nome).pipe(
-        switchMap((professor) => {
-            this.professor = professor;
-            
-            // Mapeia os IDs dos cursos para uso posterior
-            this.cursosids = this.professor.coursesId.map((curso: any) => curso._id);
-            
-            // Atualiza o professor com os IDs dos cursos
-            this.professor.coursesId = this.cursosids;
-            
-            // Configura o título da página
-            this.titulo.setTitle(`Editar Professor ${this.professor.nome}`);
-            
-            // Retorna um Observable de listagem de cursos
-             this.cursoService.listarCursos(this.cursos);
-
-             return this.retornoObservavel;
-        })
-    ).subscribe(
-        (cursos) => {
-            this.cursos = cursos;
-        },
-        (error) => {
-            console.error('Erro ao carregar cursos:', error);
-        }
-    );
-}
 
 
 //Método antigo...
-// ngOnInit(): void {
+ ngOnInit(): void {
     
     
-//   const nome = this.route.snapshot.paramMap.get('nome') ?? '';
+   const nome = this.route.snapshot.paramMap.get('nome') ?? '';
   
-//   //Sobreescreve os campos de input com o que o usuário clicou na home
-//   this.professorService.carregaProfessorPeloNome(nome).subscribe(
-//     (professor) => {
-//       this.professor = professor
+   //Sobreescreve os campos de input com o que o usuário clicou na home
+   this.professorService.carregaProfessorPeloNome(nome).subscribe(
+     (professor) => {
+       this.professor = professor
 
-//       //gambiarra para funcionar no back
-//       this.cursosids = this.professor.coursesId.map((curso:any) => curso._id)
-//       this.professor = {
-//         ...this.professor, coursesId:this.cursosids
-//       }
+       //gambiarra para funcionar no back
+       this.cursosids = this.professor.coursesId.map((curso:any) => curso._id)
+       this.professor = {
+         ...this.professor, coursesId:this.cursosids
+       }
       
 
-//     },
-//     (error) => {
-//       console.error(error);
-//     })
+     },
+     (error) => {
+       console.error(error);
+     })
   
-//   this.titulo.setTitle(`Editar Professor`)
-//   this.cursoService.listarCursos(this.cursos);
+   this.titulo.setTitle(`Editar Professor`)
+   this.cursoService.listarCursos(this.cursos);
 
   
   
 
 
   
-// }
+ }
 
 
 
